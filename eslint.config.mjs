fcba7babd316eab171f451d52433a92b6fa35851
ZFCrow <2302623@sit.singaporetech.edu.sg>
2025-07-02 12:23:55 +0800

    securtiy checks with eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-
+import security from "eslint-plugin-security";
+import noUnsanitized from "eslint-plugin-no-unsanitized";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  // ESLint's recommended rules
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      security,
+      "no-unsanitized": noUnsanitized, // ✅ match the rule prefix,
+    },
+    rules: {
+      ...security.configs.recommended.rules,
+      ...noUnsanitized.configs.recommended.rules,
+    },
+  },
 ]);
