ddc773b15a83d076eaaa6710127db2dc32f69eef
ZFCrow <2302623@sit.singaporetech.edu.sg>
2025-07-02 12:42:12 +0800

    secc node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 import security from "eslint-plugin-security";
 import noUnsanitized from "eslint-plugin-no-unsanitized";
+import pluginSecurityNode from "eslint-plugin-security-node";
+
 
 export default defineConfig([
   // ESLint's recommended rules
@@ -19,10 +21,12 @@ export default defineConfig([
     plugins: {
       security,
       "no-unsanitized": noUnsanitized, // ✅ match the rule prefix,
+      "security-node": pluginSecurityNode,
     },
     rules: {
       ...security.configs.recommended.rules,
       ...noUnsanitized.configs.recommended.rules,
+      ...pluginSecurityNode.configs.recommended.rules, // ✅ Add this line
     },
   },
 ]);
